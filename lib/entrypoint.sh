9a1fc39b6b116b445d4d4001c448858fdeea990f
Stuart Yamartino <stuart@dockwa.com>
2021-03-16 16:57:46 -0400

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-gem install rubocop
+gem install rubocop -v 0.92.0
+gem install rubocop-rspec -v 1.38.1
+gem install rubocop-performance -v 1.5.2
+gem install rubocop-rails -v 2.4.2
 
 ruby /action/lib/index.rb
